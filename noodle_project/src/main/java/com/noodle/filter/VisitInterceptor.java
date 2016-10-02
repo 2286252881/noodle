c6df1cd363797e3a55f7ca165f9e1cb9dcc4ebd1
2286252881 <2286252881@qq.com>
2016-10-03 00:32:45 +0800

    www.woaichinoodle.com

--- noodle_project/src/main/java/com/noodle/filter/VisitInterceptor.java
+++ noodle_project/src/main/java/com/noodle/filter/VisitInterceptor.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-public class LoginInterceptor implements HandlerInterceptor {
+public class VisitInterceptor implements HandlerInterceptor {
 
 	@Override
 	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
